c9e6e1eb508143ad701997ed8fca56ec0de42581
Wagner de Paula <wagnersouzadepaula@gmail.com>
2022-06-22 15:36:05 -0300

    :art: Crud finalizado e organização do codigo
    Foram realizados os CRUDS restantes: READ, UPDATE E DELETE no cadastro de livros. Os códigos foram reorganizados para um melhor entendimento.

--- app/cadastro_livro.js
+++ app/cadastro_livro.js
@@ -8,8 +8,9 @@ const conexao = {
     database: 'biblioteca'
 };
 
-/*INICIANDO CADASTRAR LIVRO */
-
+/*-------------------------------
+|      CADASTRAR    LIVRO       |
+-------------------------------*/
 function cadastrarLivro(livro, callback) {
     const cliente = new Client(conexao);
     cliente.connect();
@@ -24,9 +25,61 @@ function cadastrarLivro(livro, callback) {
         })
 }
 
+/*-------------------------------
+|       LISTAR     LIVROS       |
+-------------------------------*/
+function listar(callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT livros.idlivro, livros.isbnlivro, livros.titulolivro, autor.nomeautor as autor,livros.editoralivro, livros.anolivro, qtdelivrodisponivel FROM livros, autor WHERE livros.idautor = autor.idautor;";
+    cliente.query(sql, 
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);
+            }
+            else {
+                let livros = res.rows;
+                callback(undefined, livros);     
+            }
+            cliente.end();
+        }
+    )    
+}
 
-/*INICIANDO CONSULTAR DISPONIBILIDADE DE LIVRO*/
+/*-------------------------------
+|    BUSCAR  LIVROS  POR  ID    |
+-------------------------------*/
+function buscarPorId(id, callback){
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT livros.idlivro, livros.isbnlivro, livros.titulolivro, autor.nomeautor as autor,livros.editoralivro, livros.anolivro, qtdelivrodisponivel FROM livros, autor WHERE livros.idautor = autor.idautor AND livros.idlivro = $1";
+    const values = [id];
 
+    cliente.query(sql, values,
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);                
+            }
+            else if (res.rows && res.rows.length > 0) {
+                let produto = res.rows[0];
+                callback(undefined, produto);
+            }
+            else {
+                const error = "Produto nao encontrado";
+                callback(error, undefined);
+            }
+
+            cliente.end();
+        }
+    )    
+}
+
+/*-------------------------------
+|  CONSULTAR    DISPONIBILIDADE |
+|           DE LIVRO            |
+-------------------------------*/
 function pesquisarDisponibilidade(id, callback){
     const cliente = new Client(conexao);
     cliente.connect();
@@ -53,9 +106,62 @@ function pesquisarDisponibilidade(id, callback){
     )    
 }
 
+/*-------------------------------
+| ATUALIZAR UM LIVRO CADASTRADO |
+-------------------------------*/
+function atualizar(idlivro,livro, callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
 
+    const sql = "UPDATE livros SET isbnlivro=$1, titulolivro=$2, idautor=$3, editoralivro=$4, anolivro=$5, qtdelivrodisponivel=$6 WHERE idlivro=$7 RETURNING *"
+    const values = [livro.isbnlivro, livro.titulolivro, livro.idautor, livro.editoralivro, livro.anolivro, livro.qtdelivrodisponivel, idlivro];
+
+    cliente.query(sql, values, function(err, res) {
+        if(err) {
+            callback(err.message, undefined);                
+        }
+        else if (res.rows && res.rows.length > 0) {
+            let livro = res.rows[0];
+            callback(undefined, livro);
+        }
+        else {
+            const error = "Produto nao encontrado";
+            callback(error, undefined);
+        }
+
+        cliente.end();        
+    })
+}
+
+
+/*-------------------------------
+|    DELETAR  LIVROS  POR  ID   |
+-------------------------------*/
+function deletar(id, callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
+
+    const sql = "DELETE FROM livros WHERE idlivro=$1 RETURNING *"
+    const values = [id];
+
+    cliente.query(sql, values, function(err, res) {
+        if(err) {
+            callback(err.message, undefined);                
+        }
+        else if (res.rows && res.rows.length > 0) {
+            let livro = res.rows[0];
+            callback(undefined, livro);
+        }
+        else {
+            const error = "Livro não encontrado";
+            callback(error, undefined);
+        }
+        cliente.end();        
+    })
+
+}
 
 
 module.exports = {
-    cadastrarLivro, pesquisarDisponibilidade
+    cadastrarLivro, pesquisarDisponibilidade, listar, buscarPorId, atualizar, deletar
 };
--- app/main.js
+++ app/main.js
@@ -10,10 +10,16 @@ const emprestimoDeLivro = require('./emprestimoDeLivros')
 
 //Main
 
-/*CADASTRAR AUTOR*/
+/*############################################################################# 
+######################################  AUTORES  ##############################
+#############################################################################/*
+
+/*###############################
+#      CADASTRAR    AUTOR       #
+###############################*/
 /*
-cadastroautor.cadastrarAutor({nomeautor: "Andre Aquiles", nacionalidade:'Brasileiro'}, function(err, autorInserido) {
-        console.log("Kathy Sierra")
+cadastroautor.cadastrarAutor({nomeautor: "Rodrigo Cesar rebello Pinho", nacionalidade:'Brasileiro'}, function(err, autorInserido) {
+        console.log("")
             if(err) {
             console.log("Sistema esta com problemas");
             console.log(err);
@@ -26,7 +32,54 @@ cadastroautor.cadastrarAutor({nomeautor: "Andre Aquiles", nacionalidade:'Brasile
 );
 */
 
-//CADASTRAR LIVRO
+
+
+
+/*############################################################################# 
+######################################  ALUNOS  ###############################
+#############################################################################/*
+
+/*-------------------------------
+|      CADASTRAR    ALUNO       |
+-------------------------------*/
+/*
+cadastroAluno.cadastrarAluno({matriculaaluno: 123, nomealuno: "Aureliano Buendia", telefonealuno: '51999887766'}, function(err, alunoinserido){
+    if (err) {
+        console.log("Erro ao inserir o aluno.")
+    }
+    else{
+        console.log(alunoinserido)
+    }
+})
+*/
+
+
+/*############################################################################# 
+###################################### USUÁRIOS ###############################
+#############################################################################/*
+
+/*-------------------------------
+|      CADASTRAR   USUARIO      |
+-------------------------------*/
+
+/*
+cadastroUsuario.cadastrarUsuarios({nomeusuario: "Wagner", senhausuario: 12345}, function(err,res){
+    if (err) {
+        console.log("Cadastro de usuários com problemas");
+    }
+    else{
+        console.log(res)
+    }
+})
+*/
+
+/*############################################################################# 
+######################################  LIVROS  ###############################
+#############################################################################/*
+
+/*-------------------------------
+|      CADASTRAR    LIVRO       |
+-------------------------------*/
 /*
 cadastroLivro.cadastrarLivro({isbnlivro: '9788566250121', titulolivro: 'Agile Desenvolvimento de software com entregas frequentes e foco no valor de negócio', idautor: 4, editoralivro: 'Casa do Código', anolivro: '2013-01-01', qtdelivrodisponivel: 1}, function(err, livroInserido){
     console.log("Inserindo livro...")
@@ -40,7 +93,49 @@ cadastroLivro.cadastrarLivro({isbnlivro: '9788566250121', titulolivro: 'Agile De
 })
 */
 
-/*CONSULTAR DISPONIBILIDADE DE LIVRO*/
+
+/*-------------------------------
+|       LISTAR     LIVROS       |
+-------------------------------*/
+/*
+cadastroLivro.listar(
+    function(err, livro) {
+        console.log("Listar: ");
+        if(err) {
+            console.log("Sistema esta com problemas");
+            console.log(err);
+        }
+        else {
+            console.log(livro);
+
+        }
+    }
+);
+*/
+
+
+/*-------------------------------
+|    BUSCAR  LIVROS  POR  ID    |
+-------------------------------*/
+/*
+cadastroLivro.buscarPorId(4, 
+    function(erro, livros) {
+        console.log("BuscarPorId(2): ");
+        if(erro) {
+            console.log("Sistema está com problemas");
+            console.log(erro);
+        }
+        else {
+            console.log(livros);
+        }
+});
+*/
+
+
+/*-------------------------------
+|  CONSULTAR    DISPONIBILIDADE |
+|           DE LIVRO            |
+-------------------------------*/
 /*
 cadastroLivro.pesquisarDisponibilidade(3, function (err, res){
     console.log("consultando quantidade de livro disponível do id 3");
@@ -54,32 +149,49 @@ cadastroLivro.pesquisarDisponibilidade(3, function (err, res){
 })
 */
 
-/* CADASTRAR ALUNO */
+
+/*-------------------------------
+| ATUALIZAR UM LIVRO CADASTRADO |
+-------------------------------*/
 /*
-cadastroAluno.cadastrarAluno({matriculaaluno: 123, nomealuno: "Aureliano Buendia", telefonealuno: '51999887766'}, function(err, alunoinserido){
-    if (err) {
-        console.log("Erro ao inserir o aluno.")
-    }
-    else{
-        console.log(alunoinserido)
-    }
-})
+cadastroLivro.atualizar(8, {isbnlivro: '9788566250121', titulolivro: 'Agile Desenvolvimento de software com entregas frequentes e foco no valor de negócio edição atualizada', idautor: 4, editoralivro: 'Casa do Código', anolivro: '2013-01-01', qtdelivrodisponivel: 1}, 
+    function(erro, produto) {
+        console.log("Atualizar Produto(1): ");
+        if(erro) {
+            console.log("Erro ao atualizar livro");
+            console.log(err);
+        }
+        else {
+            console.log(produto);
+        }
+});
 */
 
-/* CADASTRAR USUARIO */
+
+/*-------------------------------
+|    DELETAR  LIVROS  POR  ID   |
+-------------------------------*/
 /*
-cadastroUsuario.cadastrarUsuarios({nomeusuario: "Wagner", senhausuario: 12345}, function(err,res){
-    if (err) {
-        console.log("Cadastro de usuários com problemas");
+cadastroLivro.deletar(7, function(erro, livro) {
+    console.log("Livro deletado (7): ");
+    if(erro) {
+        console.log("Sistema está com problemas");
+        console.log(erro);
     }
-    else{
-        console.log(res)
+    else {
+        console.log(livro);
     }
-})
+});
 */
 
-/* EMPRESTAR UM LIVRO */
+/*############################################################################# 
+##############################  EMPRÉSTIMO DE LIVROS  #########################
+#############################################################################/*
 
+/*-------------------------------
+|       EMPRESTAR UM LIVRO      |
+-------------------------------*/
+/*
 emprestimoDeLivro.emprestarLivro({idlivro: 3, idaluno: 1, dataemprestimo:'2022-06-13', idusuario: 1}, function(err, res){
     if (err) {
         console.log("Sistema de empréstimo de livros está com problemas");
@@ -88,3 +200,4 @@ emprestimoDeLivro.emprestarLivro({idlivro: 3, idaluno: 1, dataemprestimo:'2022-0
         console.log(res)
     }
 })
+*/
